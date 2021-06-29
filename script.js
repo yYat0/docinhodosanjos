674a40ef69e6f585e7779da03108ad3bc839df9f
Yat0 <44180656+yYat0@users.noreply.github.com>
2021-06-29 15:51:36 -0300

    Ajustes Enormes no site

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "20px 10px";
-    document.getElementById("logo").style.fontSize = "25px";
+    document.getElementById("navbar").style.padding = "15px 10px";
+    document.getElementById("logo").style.fontSize = "20px";
   } else {
-    document.getElementById("navbar").style.padding = "25px 10px";
-    document.getElementById("logo").style.fontSize = "29px";
+    document.getElementById("navbar").style.padding = "20px 10px";
+    document.getElementById("logo").style.fontSize = "22px";
   }
 }
